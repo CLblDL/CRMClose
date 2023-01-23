bfa299143d887ac2bf0f1691d50a97d3797a8a76
CLblDL <denil.lobanov1994@gmail.com>
2023-01-24 02:24:08 +0300

    Добавьте файлы проекта.

---
+++ Notes.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+
+namespace Requests.Application.Common.Mappings
+{
+    public interface IMapWith<T>
+    {
+        void Mapping(Profile profile) =>
+            profile.CreateMap(typeof(T), GetType());
+    }
+}
